23272f90ab570c0b1b2bd5a2b5f753464ecd1d60
MahdyZ7 <amahdy9@gamil.com>
2024-02-29 21:15:23 +0400

    mv to computer

--- srcs/requirements/nginxDeb/tools/nginx-set.sh
+++ srcs/requirements/nginxDeb/tools/nginx-set.sh
@@ -3,22 +3,24 @@
 mkdir /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx-selfsigned.key -out /etc/nginx/ssl/nginx-selfsigned.crt -subj "/CN=localhost"
 
-echo '
-	server {
-		listen			443 ssl http2;
-		listen			[::]:443 ssl http2;
-		ssl_certificate	/etc/nginx/ssl/nginx-selfsigned.crt;
-		ssl_certificate_key /etc/nginx/ssl/nginx-selfsigned.key;
-		ssl_protocols TLSv1.2 TLSv1.3;
-		root /var/www/html;
-		index index.html index.htm index.nginx-debian.html;
-		server_name localhost ayassin.42.fr;
-		location / {
-			try_files $uri $uri/ =404;
-		}
-		location ~ \.php$ {
-			include snippets/fastcgi-php.conf;
-			fastcgi_pass wordpress:9000;
-		}
-	' > /etc/nginx/sites-available/default
+echo 'server {
+				listen			443 ssl http2;
+				listen			[::]:443 ssl http2;
+				server_name localhost ayassin.42.fr;
+				root /var/www/html;
+				index index.html index.htm index.nginx-debian.html;
+				ssl_certificate	/etc/nginx/ssl/nginx-selfsigned.crt;
+				ssl_certificate_key /etc/nginx/ssl/nginx-selfsigned.key;
+				ssl_protocols TLSv1.2 TLSv1.3;
+				location / {
+					try_files $uri $uri/ /index.php?$args =404;
+				}
+				location ~ \.php$ {
+					include snippets/fastcgi-php.conf;
+					fastcgi_pass wordpress:9000;
+				}
+		}' > /etc/nginx/sites-available/default
+
+nginx -g "daemon off;"
+
 
--- srcs/requirements/wordpress/tools/wordpress-set.sh
+++ srcs/requirements/wordpress/tools/wordpress-set.sh
@@ -1,12 +1,13 @@
 #!bin/bash
 
 cd /var/www/html/wordpress
-wp core download --path=/var/www/html --allow-root
-wp core config --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=mariadb:3306 --path=/var/www/html --allow-root
 
-if ! wp core is-installed --path=/var/www/html --allow-root; then
+wp core download --force --allow-root
+wp config create --path=/var/www/html --allow-root --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=mariadb:3306 --dbprefix=wp_
+
+if ! wp core is-installed --allow-root --path=/var/www/html; then
 	wp core install --url=ayassin.42.fr --title="ayassin" --admin_user=ayassin --admin_password=ayassin --admin_email="gmai;@gamil.com" --allow-root
 	wp user create mkhan mkhan@gmail.com --role=author --user_pass=mkhan --allow-root
 fi
 
-php-fpm7.4 -FR
+php-fpm7.4 -F
